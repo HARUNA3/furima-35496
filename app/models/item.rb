e9a4895d9b38cd293858a9a713bcea43b37152b1
HARUNA3 <ringo144516@gmail.com>
2021-07-20 20:56:02 +0900

    setting validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,17 +8,19 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :user
+    validates :image
     validates :name,       length: { maximum: 40 }
     validates :item_info,  length: { maximum: 1000 }
+    validates :prefecture_id
     with_options numericality: { other_than: 1 } do
       validates :item_category_id
       validates :item_sales_status_id
       validates :item_shipping_fee_status_id
-      validates :prefecture_id
       validates :item_scheduled_delivery_id
     end
   end
-  #validates :item_price numericality: { 300..9999999 }
+  
+  validates :item_price, numericality: { only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   belongs_to :user
   has_one_attached :image
