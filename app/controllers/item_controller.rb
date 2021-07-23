03018c3d27218f30054d2fa2ec000e31d7c099ff
HARUNA3 <ringo144516@gmail.com>
2021-07-23 23:03:12 +0900

    fix destroy-method

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,7 +1,7 @@
 class ItemController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user?, only: [:edit, :update]
+  before_action :correct_user?, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,10 +35,8 @@ class ItemController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user_id
-      @item.destroy
-      redirect_to root_path
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
