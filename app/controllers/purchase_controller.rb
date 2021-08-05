2e1dc8dbb2cf1ac45c46c20bed8e33e64d85d953
HARUNA3 <ringo144516@gmail.com>
2021-08-05 19:55:03 +0900

    create table

---
+++ app/controllers/purchase_controller.rb
@@ -0,0 +1,14 @@
+class PurchaseController < ApplicationController
+
+  def index
+  end
+  
+  
+  def new
+    
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "item#index"
   resources :item
+  resources :purchase, only: [:new, :create, :index]
 end
